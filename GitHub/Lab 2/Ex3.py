238e12e6e25e2166c39355b57a23fc00bd73de8b
AndrewLin8 <alin8@hawaii.edu>
2026-01-22 12:25:27 -1000

    Ex3.py

---
+++ GitHub/Lab 2/Ex3.py
@@ -0,0 +1,11 @@
+# Ask the user to enter a floating point number. Square the number 
+# Print the orginal number and the squared number.
+# Name: Andrew Lin
+# Date: Jan, 22, 2026
+
+input_value = input("Please enter a floating point number")
+float_value = float(input_value)
+squared_value = float_value ** 2
+
+print ("you entered', float_value")
+print ("The squared value is (squared_value)")
